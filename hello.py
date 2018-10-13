f928c3761fd6f5662fae0c4e8241530f75be81ba
MelisaArceA <arce.melisa@outlook.es>
2018-10-13 14:49:41 -0500

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+print ("Hello world")
+a="Hello World" 
+b=5
+c=3.15
+d="a"   # char and string are the same  
+print (a)
+
+#statments
+if c > 5: #== igual, <, = asigment, 
+	print ("Larger")
+else:
+	print ("Smaller")
+#cd django 	
+#Activate virtual environtment: myvenv\Scripts\activate
+
+
+#shutup server= python manage.py runserver
+#http://127.0.0.1:8000/
